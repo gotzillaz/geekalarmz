bf501d1e73c1e6b9c99cf48c070e02ecab617650
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-02-26 00:12:02 +0700

    can select time to awake

--- app/src/main/java/net/kodyx/gotzillaz/geekalarmz/MainActivity.java
+++ app/src/main/java/net/kodyx/gotzillaz/geekalarmz/MainActivity.java
@@ -3,10 +3,13 @@ package net.kodyx.gotzillaz.geekalarmz;
 import android.app.AlarmManager;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.SystemClock;
 import android.os.Vibrator;
@@ -14,6 +17,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,6 +40,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     private AlarmManager mAlarmManager;
     private Intent intent;
     private PendingIntent pendingIntent;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private NotificationManager mNotiManager;
     public int hour = 0;
     public int minute = 0;
 
@@ -44,9 +51,23 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         public void onTimeSet(TimePicker view, int hourOfDaySet, int minuteSet) {
             hour = hourOfDaySet;
             minute = minuteSet;
+            mEditor = mSharedPreferences.edit();
+            mEditor.putInt("hour", hourOfDaySet);
+            mEditor.putInt("minute", minuteSet);
+            mEditor.apply();
             mTimeTextView.setText(String.format("%02d:%02d", hour, minute));
         }
     };
+
+    @Override
+    protected  void onNewIntent(Intent it){
+        super.onNewIntent(it);
+        Log.d("enableStatus", "onNewIntent");
+        boolean enableStatus = it.getBooleanExtra("isEnable", false);
+        mEnableSwitch = (SwitchCompat) findViewById(R.id.switch_enable);
+        mEnableSwitch.setChecked(enableStatus);
+        Log.d("enableStatus", enableStatus+"");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,12 +79,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         mEnableSwitch = (SwitchCompat) findViewById(R.id.switch_enable);
         mVibrateCheckBox = (CheckBox) findViewById(R.id.checkbox_vibrate);
 
-        mMediaPlayer = MediaPlayer.create(this, R.raw.massive_war_alarm);
-        mMediaPlayer.setLooping(true);
+//        mMediaPlayer = MediaPlayer.create(this, R.raw.massive_war_alarm);
+//        mMediaPlayer.setLooping(true);
 
         mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         intent = new Intent(this, AlarmActivity.class);
         pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mSharedPreferences = getSharedPreferences("alarm", Context.MODE_PRIVATE);
+        hour = mSharedPreferences.getInt("hour", 0);
+        minute = mSharedPreferences.getInt("minute", 0);
+
+        mTimeTextView.setText(String.format("%02d:%02d", hour, minute));
+        mEnableSwitch.setChecked(getIntent().getBooleanExtra("isEnable",false));
+
+        mNotiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         mTimeTextView.setOnClickListener(this);
         mEnableSwitch.setOnCheckedChangeListener(this);
@@ -104,8 +134,23 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         if(buttonView.getId() == R.id.switch_enable) {
             Toast.makeText(this, "SWITCH "+isChecked, Toast.LENGTH_SHORT).show();
             if(isChecked) {
-                mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 5000, pendingIntent);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, minute);
 
+                mAlarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                Intent notiIntent = new Intent(this, MainActivity.class);
+                notiIntent.putExtra("isEnable", true);
+                PendingIntent notiPendingIntent = PendingIntent.getActivity(this, 1, notiIntent, 0);
+                Notification.Builder notiBuilder = new Notification.Builder(this);
+                notiBuilder.setSmallIcon(R.drawable.ic_alarm_white_48dp)
+                    .setContentTitle("Upcoming GeekAlarmZ")
+                    .setContentText(String.format("%02d:%02d", hour, minute))
+                    .setContentIntent(notiPendingIntent);
+                Notification notification = notiBuilder.build();
+                notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;;
+                mNotiManager.notify(0, notification);
 //                mMediaPlayer.start();
 //                Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
 //                // Vibrate for 500 milliseconds
@@ -113,6 +158,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             }
             else {
                 mAlarmManager.cancel(pendingIntent);
+                mNotiManager.cancel(0);
 //                mMediaPlayer.pause();
 //                mMediaPlayer.seekTo(0);
             }
